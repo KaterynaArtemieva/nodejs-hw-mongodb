d5ac4333e909c13c864a1636b9beabf038c1d1ed
KaterynArtemieva <moruga.katya@gmail.com>
2024-10-05 00:10:54 +0100

    done

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
 
 export const setupServer = () => {
 
@@ -25,6 +27,35 @@ export const setupServer = () => {
           message: 'Hello world!',
         });
       });
+
+      app.get('/contacts', async (req, res) => {
+        const contacts = await getAllContacts();
+    
+        res.status(200).json({
+            status: 200,
+            message: "Successfully found contacts!",
+            data: contacts,
+        });
+      });
+
+      app.get('/contacts/:contactId', async (req, res) => {
+        const { contactId } = req.params;
+          const contact = await getContactById(contactId);   
+          
+          if (!contact) {
+            res.status(404).json({
+                status: 404,
+                message: 'Contact not found'
+            });
+            return;
+          }
+      
+          res.status(200).json({
+            status: 200,
+            message: `Successfully found contact with id ${contactId}!`,
+            data: contact,
+          });
+        });
 
     app.use('*', (req, res, next) => {
         res.status(404).json({
